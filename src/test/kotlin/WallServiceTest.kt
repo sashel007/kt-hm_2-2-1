8ad6d2cf2e7c943b8b066cfe8bf4e3130e7f41d9
Alexander Sidorkin <sashel007@yandex.ru>
2023-06-14 21:58:15 +0300

    add CI

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,3 +1,4 @@
+import PostObjects.*
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -6,6 +7,7 @@ class WallServiceTest {
     @Test
     fun add() {
         val wallService = WallService()
+        val attachments: Array<Attachment> = arrayOf(PhotoAttachment(Photo(1,1,"","")))
         val post = Post(
             20,
             1,
@@ -16,65 +18,130 @@ class WallServiceTest {
             1,
             1,
             true,
-            "",
             Comment(),
-            Like()
+            Like(),
+            Copyright(),
+            Repost(),
+            View(),
+            "",
+            PostSource(),
+            Geo(),
+            1,
+            copyHistory = arrayOf(1),
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            Donut(),
+            1,
+            attachments = attachments
         )
+
         val addedPostResult = wallService.add(post)
 
-        assertEquals(0,addedPostResult.id)
+        assertEquals(1,addedPostResult.id)
     }
 
     @Test
     fun update() {
         val wallService = WallService()
+        val attachments: Array<Attachment> = arrayOf(PhotoAttachment(Photo(1,1,"","")))
         val post = Post(
             1,
             1,
             1,
             1,
-            123456789,
-            "Test post",
-            0,
-            0,
-            false,
+            1,
             "",
+            1,
+            1,
+            true,
             Comment(),
-            Like()
+            Like(),
+            Copyright(),
+            Repost(),
+            View(),
+            "",
+            PostSource(),
+            Geo(),
+            1,
+            copyHistory = arrayOf(1),
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            Donut(),
+            1,
+            attachments = attachments
         )
         wallService.add(post)
-
         // Изменяем пост с существующим id, ожидаем true
         val updatedPost1 = Post(
             1,
             1,
             1,
             1,
-            123456789,
-            "Updated post",
-            0,
-            0,
-            false,
+            1,
             "",
+            1,
+            1,
+            true,
             Comment(),
-            Like()
+            Like(),
+            Copyright(),
+            Repost(),
+            View(),
+            "",
+            PostSource(),
+            Geo(),
+            1,
+            copyHistory = arrayOf(1),
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            Donut(),
+            1,
+            attachments = attachments
         )
         assertTrue(wallService.update(updatedPost1))
 
         // Изменяем пост с несуществующим id, ожидаем false
         val updatedPost2 = Post(
             2,
-            2,
-            2,
-            2,
-            123456789,
-            "Updated post",
-            0,
-            0,
-            false,
+            1,
+            1,
+            1,
+            1,
             "",
+            1,
+            1,
+            true,
             Comment(),
-            Like()
+            Like(),
+            Copyright(),
+            Repost(),
+            View(),
+            "",
+            PostSource(),
+            Geo(),
+            1,
+            copyHistory = arrayOf(1),
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            Donut(),
+            1,
+            attachments = attachments
         )
         assertFalse(wallService.update(updatedPost2))
     }
